a2cf74789977a2138eba456ab0d0de59e82ec229
johary1 <joharisoa@gmail.com>
2022-11-30 10:12:37 +0100

    fix modal display

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,23 +8,28 @@ function editNav() {
 }
 
 // DOM Elements
+//2 modals
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalbgConfirm = document.querySelector(".bgroundConfirm");
+
 const formData = document.querySelectorAll(".formData");
 const modalContent = document.querySelector(".content");
-
+const modalContentConfirm = document.querySelector(".contentConfirm");
+const modalBtnSignup = document.getElementById("signup");
+const form = document.querySelector("form");
 const first = document.querySelector("#first");
 const last = document.querySelector("#last");
 const email = document.querySelector("#email");
 const checkbox = document.querySelector("#checkbox1");
-
+let firstName, lastName, emailAdress;
 console.log(first + "" + last + "" + email);
 const inputs = document.querySelectorAll(
   'input[type="text"]',
   'input[type="checkbox"]'
 );
 
-const form = document.querySelector("form");
+const btnSubmit = document.getElementById("submitbtn");
+console.log(btnSubmit);
 // steps to check all inputs to be valid
 const isValid = (value) => (value === "" ? false : true);
 const isBetween = (length, min, max) =>
@@ -70,19 +75,25 @@ const checkUsername = () => {
 
   if (!isValid(firstname)) {
     showError(first, "Ce champ doit être rempli");
+    firstName = null;
   } else if (!isBetween(firstname.length, min, max)) {
     showError(first, `Veuillez saisir entre ${min} et ${max} caractères.`);
+    firstName = null;
   } else {
     showSuccess(first);
+    firstName = firstname;
     valid = true;
   }
 
   if (!isValid(lastname)) {
     showError(last, "Ce champ doit être rempli");
+    lastName = null;
   } else if (!isBetween(lastname.length, min, max)) {
     showError(last, `Veuillez saisir entre ${min} et ${max} caractères.`);
+    lastName = null;
   } else {
     showSuccess(last);
+    lastName = lastname;
     valid = true;
   }
   return valid;
@@ -93,10 +104,13 @@ const checkEmail = () => {
   const emailValue = email.value.trim();
   if (!isValid(emailValue)) {
     showError(email, "Ce champ doit être rempli");
+    emailAdress = null;
   } else if (!isEmailValid(emailValue)) {
     showError(email, "L'adresse email n'est pas valide");
+    emailAdress = null;
   } else {
     showSuccess(email);
+    emailAdress = emailValue;
     valid = true;
   }
   return valid;
@@ -115,36 +129,53 @@ const checkCheckboxInput = () => {
   return valid;
 };
 
-const close = document.getElementById("close");
 // close modal form
+const close = document.getElementById("close");
+
 close.addEventListener("click", () => {
   modalbg.style.display = "none";
   document.body.style.overflow = "initial";
 });
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal form
-function launchModal() {
+let showFirstModal = false;
+const launchModal = () => {
   modalbg.style.display = "block";
   document.body.style.overflow = "hidden";
-}
+  showFirstModal = true;
+};
+
+// launch modal event
+modalBtnSignup.addEventListener("click", launchModal);
 
 // launch modal confirmation registration
-function launchModalConfirmRegistration() {
-  modalbg.style.display = "block";
-  modalContent.textContent = "";
-  modalContent.innerHTML += `<span class="closeRegister" id="close-register"></span>
+const launchModalConfirmRegistration = () => {
+  modalbgConfirm.style.display = "block";
+  if (showFirstModal) {
+    modalbg.style.display = "none";
+  }
+  modalContentConfirm.innerHTML += `<span class="closeRegister" id="close-register"></span>
   <div class="endingTxt">Merci pour votre inscription</div>
   <input
   class="btn-register"
-  type="submit"
-  class="button"
   id="close-confirm"
   value="Fermer"
+  type="submit"
+
 />`;
-}
+  const closeRegister = document.getElementById("close-register");
+  const btnConfirm = document.getElementById("close-confirm");
+  // close modal register form
+  closeRegister.addEventListener("click", () => {
+    modalbgConfirm.style.display = "none";
+    document.body.style.overflow = "initial";
+  });
+  btnConfirm.addEventListener("click", () => {
+    modalbgConfirm.style.display = "none";
+    document.body.style.overflow = "initial";
+  });
+};
+
 // check input in real time
 inputs.forEach((input) => {
   input.addEventListener("input", (e) => {
@@ -164,32 +195,40 @@ inputs.forEach((input) => {
     }
   });
 });
+//clear inputs
+function clearInputs() {
+  inputs.forEach((input) => (input.value = ""));
+}
 // form submit
-form.addEventListener("submit", (e) => {
+btnSubmit.addEventListener("click", (e) => {
   //prevent from form submission
   e.preventDefault();
   // all conditions must be true
+  if (firstName && lastName && emailAdress) {
+    const data = {
+      firstName,
+      lastName,
+      emailAdress,
+    };
+    console.log(
+      "res = " + data.emailAdress + " " + data.firstName + " " + data.lastName
+    );
+  }
   // validate forms
   let isUsernameValid = checkUsername(),
     isCGUchecked = checkCheckboxInput(),
     isEmailValid = checkEmail();
 
   let isFormValid = isUsernameValid && isEmailValid && isCGUchecked;
-
   // submit to the server if the form is valid
   if (isFormValid) {
     // launch modal register form
     launchModalConfirmRegistration();
-    const closeRegister = document.getElementById("close-register");
-    const btnConfirm = document.getElementById("close-confirm");
-    // close modal register form
-    closeRegister.addEventListener("click", () => {
-      modalbg.style.display = "none";
-      document.body.style.overflow = "initial";
-    });
-    btnConfirm.addEventListener("click", () => {
-      modalbg.style.display = "none";
-      document.body.style.overflow = "initial";
-    });
+
+    clearInputs();
+
+    btnSubmit.disabled = true;
+    alert("vous vous êtes déjà inscrit(e)");
+    return true;
   }
 });
